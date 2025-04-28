bd316de5b9c6e0736d3ffa9021d14ee9bed1332b
AdrianS2002 <aschiop2002@yahoo.com>
2025-04-29 01:00:37 +0300

    added the logic and style for targetprice tab

--- src/app/target-price/target-price.component.ts
+++ src/app/target-price/target-price.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { PairPriceService } from '../services/pairPrice.service';
 
 @Component({
   selector: 'app-target-price',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './target-price.component.html',
   styleUrl: './target-price.component.css'
 })
 export class TargetPriceComponent {
+  @Input() selectedPair!: string;
+  isLong = true;
+  leverage = 20;
+  entryPrice!: number;
+  targetPrice!: number;
+  exitPrice!: number;
+  roi!: number
+  constructor(private pairPriceService: PairPriceService) { }
 
+  selectPosition(position: 'long' | 'short') {
+    this.isLong = (position === 'long');
+  }
+
+  getPrice(target: 'entry' | 'exit') {
+    if (!this.selectedPair) return;
+
+    this.pairPriceService.getCurrentPrice(this.selectedPair).subscribe({
+      next: (price) => {
+        if (target === 'entry') {
+          this.entryPrice = price;
+        } else if (target === 'exit') {
+          this.exitPrice = price;
+        }
+      },
+      error: (error) => console.error('Error fetching price:', error)
+    });
+  }
+
+  calculateTarget() {
+    if (!this.entryPrice || !this.roi || !this.leverage) return;
+  
+    const initialMargin = this.entryPrice / this.leverage;
+    const pnlTarget = (this.roi / 100) * initialMargin;
+  
+    if (this.isLong) {
+      this.targetPrice = parseFloat((this.entryPrice + pnlTarget).toFixed(2));
+    } else {
+      this.targetPrice = parseFloat((this.entryPrice - pnlTarget).toFixed(2));
+    }
+  }
+  
 }
