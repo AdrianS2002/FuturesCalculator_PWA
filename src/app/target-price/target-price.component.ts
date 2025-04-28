7d10ba19646ca0e028a67369d8045f300838cb42
AdrianS2002 <aschiop2002@yahoo.com>
2025-04-28 14:42:37 +0300

    added basic layout for the app

---
+++ src/app/target-price/target-price.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-target-price',
+  standalone: true,
+  imports: [],
+  templateUrl: './target-price.component.html',
+  styleUrl: './target-price.component.css'
+})
+export class TargetPriceComponent {
+
+}
