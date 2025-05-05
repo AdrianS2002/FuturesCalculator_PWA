4c036d57cab8e3d31e03782f2c60650812f125c1
AdrianS2002 <aschiop2002@yahoo.com>
2025-05-05 13:58:16 +0300

    added pwa integration

--- src/app/services/pairPrice.service.ts
+++ src/app/services/pairPrice.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map, Observable } from "rxjs";
+import { catchError, map, Observable, of } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
@@ -9,7 +9,7 @@ import { map, Observable } from "rxjs";
 export class PairPriceService {
     private apiUrl = 'https://api.binance.com/api/v3/exchangeInfo';
     private tickerPriceUrl = 'https://api.binance.com/api/v3/ticker/price';
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) { }
 
     getTradingPairs(): Observable<string[]> {
         return this.http.get<any>(this.apiUrl).pipe(
@@ -22,9 +22,22 @@ export class PairPriceService {
     }
 
     getCurrentPrice(symbol: string): Observable<number> {
+        const cacheKey = `price-${symbol}`;
+        const cached = localStorage.getItem(cacheKey);
+      
         return this.http.get<any>(`${this.tickerPriceUrl}?symbol=${symbol}`).pipe(
-            map((response: any) => parseFloat(response.price))
+          map((response: any) => {
+            const price = parseFloat(response.price);
+            localStorage.setItem(cacheKey, price.toString());
+            return price;
+          }),
+          catchError((err) => {
+            if (cached) {
+              return of(parseFloat(cached));
+            }
+            return of(NaN);
+          })
         );
-    }
+      }
 }
 
